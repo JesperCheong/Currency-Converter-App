e5affce856d83b2ab663a2aabf9c0b655bcdfb72
JesperCheong <75248011+JesperCheong@users.noreply.github.com>
2024-04-30 20:26:19 +0000

    Added fetch data from api
    - fetch from api and save in local storage
    - only fetch again if new data available from api

---
+++ script.js
@@ -0,0 +1,38 @@
+async function fetchRate() {
+  try {
+    const response = await fetch("https://v6.exchangerate-api.com/v6/75ecfe5ea43c6bf69b25929b/latest/USD");
+    const data = await response.json();
+    if (data.result === "success") {
+      console.log("data fetched from API");
+      // store data from api in local storage
+      // data from api will only update once a day, no need to keep fetch same data
+      localStorage.setItem("conversionData", JSON.stringify(data.conversion_rates));
+      localStorage.setItem("nextUpdateData", data.time_next_update_utc);
+      return data.conversion_rates;
+    } else if (data.result === "error") {
+      console.log(data["error-type"]);
+      alert(data["error-type"]);
+    }
+
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+}
+
+async function initialize() {
+  // fetch data if
+  // 1. (update time/conversion rate) is not found in local storage
+  // 2. data is expired
+  const nextAPIUpdateTime = localStorage.getItem("nextUpdateData");
+  let conversionRates = JSON.parse(localStorage.getItem("conversionData"));
+  if (!nextAPIUpdateTime || !conversionRates || new Date(nextAPIUpdateTime) < new Date()) {
+    conversionRates = await fetchRate();
+    console.log("data from api", conversionRates)
+  } else {
+    console.log("data from local storage", conversionRates)
+  }
+
+}
+
+initialize();
